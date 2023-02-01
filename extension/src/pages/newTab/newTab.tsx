4aca55f57feb5a3fb042965d738a6de5ecb6da73
엄성현 <mingyov0807@gmail.com>
2023-02-01 14:40:04 +0900

    feat : react query on newtab app

--- extension/src/pages/newTab/newTab.tsx
+++ extension/src/pages/newTab/newTab.tsx
@@ -1,31 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { createRoot } from "react-dom/client";
-import { BookmarkModal } from "@/components/BookmarkModal";
-import "@root/index.css";
-import { Bookmark } from "@/components/Bookmark";
-import { Searchbar } from "@/components/Searchbar";
-import { Settingbar } from "@/components/Settingbar";
-import { NewsBoard } from "@/components/NewsBoard";
-import { RealtimeKeyword } from "@/components/RealtimeKeyword";
-import { DashIndex } from "@/components/Modals/DashBoard";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider, useSelector, useDispatch } from "react-redux";
-import { persistor, store, AppDispatch, RootState } from "@/modules/store";
-import Scrollbars from "react-custom-scrollbars-2";
-import { SettingModal } from "@/components/SettingModal";
-import { DashboardModal } from "@/components/DashboardModal";
-import { KeywordModal } from "@/components/KeywordModal";
+import React, { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BookmarkModal } from '@/components/BookmarkModal';
+import '@root/index.css';
+import { Bookmark } from '@/components/Bookmark';
+import { Searchbar } from '@/components/Searchbar';
+import { Settingbar } from '@/components/Settingbar';
+import { NewsBoard } from '@/components/NewsBoard';
+import { RealtimeKeyword } from '@/components/RealtimeKeyword';
+import { DashIndex } from '@/components/Modals/DashBoard';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import { persistor, store, AppDispatch, RootState } from '@/modules/store';
+import Scrollbars from 'react-custom-scrollbars-2';
+import { SettingModal } from '@/components/SettingModal';
+import { DashboardModal } from '@/components/DashboardModal';
+import { KeywordModal } from '@/components/KeywordModal';
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
 
 //api
-import axios from "axios";
+import axios from 'axios';
 import {
   getNews,
   trendAPI,
   getRecommendKeywords,
   newsLogApi,
-} from "@modules/api";
-import { userStateCheck } from "@/modules/auth";
-import { getSubscribeKeywordsRedux } from "@/modules/keyWordReducer";
+} from '@modules/api';
+import { userStateCheck } from '@/modules/auth';
+import { getSubscribeKeywordsRedux } from '@/modules/keyWordReducer';
 
 const App: React.FC<{}> = () => {
   const [BookmarkModalOpen, setBookmarkModalOpen] = useState(false);
@@ -35,7 +41,7 @@ const App: React.FC<{}> = () => {
   const [bookmarkTree, setBookmarkTree] = useState<
     chrome.bookmarks.BookmarkTreeNode[]
   >([]);
-  const [imgUrl, setImgUrl] = useState("");
+  const [imgUrl, setImgUrl] = useState('');
   const { accessToken, isLogin, subScribeKwdList } = useSelector(
     (state: RootState) => {
       return {
@@ -55,14 +61,14 @@ const App: React.FC<{}> = () => {
     fetchBookmarks();
   }, [BookmarkModalOpen]);
   useEffect(() => {
-    if (accessToken !== "") {
-      axios.defaults.headers.common["Authorization"] = `${accessToken}`;
+    if (accessToken !== '') {
+      axios.defaults.headers.common['Authorization'] = `${accessToken}`;
     }
     const getInitialData = async () => {
       const jwtStatus = await dispatch(userStateCheck()).unwrap();
       // console.log("jwtStatus", jwtStatus);
-      if (accessToken !== "") {
-        axios.defaults.headers.common["Authorization"] = `${accessToken}`;
+      if (accessToken !== '') {
+        axios.defaults.headers.common['Authorization'] = `${accessToken}`;
       }
       await trendAPI();
       await getRecommendKeywords();
@@ -72,13 +78,13 @@ const App: React.FC<{}> = () => {
     getInitialData();
   }, []);
   const fetchBookmarks = () => {
-    chrome.bookmarks.getChildren("1", (bookmarkTreeNodes) => {
+    chrome.bookmarks.getChildren('1', (bookmarkTreeNodes) => {
       // console.log(bookmarkTreeNodes);
       setBookmarkTree(bookmarkTreeNodes);
     });
   };
   const fetchUrl = () => {
-    chrome.storage.local.get(["bgimg"], (result) => {
+    chrome.storage.local.get(['bgimg'], (result) => {
       setImgUrl(result.bgimg);
       // console.log(result.bgimg)
     });
@@ -88,11 +94,11 @@ const App: React.FC<{}> = () => {
       <div
         className="flex flex-col bg-cover"
         style={{
-          width: "100vw",
-          height: "100vh",
+          width: '100vw',
+          height: '100vh',
           backgroundImage: !!imgUrl
-            ? "url(" + imgUrl + ")"
-            : "url(default-background.png)",
+            ? 'url(' + imgUrl + ')'
+            : 'url(default-background.png)',
         }}
       >
         <div className="flex flex-row justify-center h-full">
@@ -154,14 +160,16 @@ const App: React.FC<{}> = () => {
     </>
   );
 };
-
-const container = document.createElement("div");
+const queryClient = new QueryClient();
+const container = document.createElement('div');
 document.body.appendChild(container);
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </PersistGate>
   </Provider>
 );
