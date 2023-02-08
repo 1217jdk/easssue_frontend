56247455453c100643ef1a194438e301c126fb75
Seonghyun Eom <seonghyuneom@Seonghyunui-MacBookPro.local>
2023-02-08 09:45:02 +0900

    update : getNewsApi v3

--- extension/src/components/NewsBoard/hooks/useGetNewsData.ts
+++ extension/src/components/NewsBoard/hooks/useGetNewsData.ts
@@ -2,14 +2,14 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { getNews, getNewsByKeyword, getRecommendNews } from '@/API/news';
 import { AxiosError } from 'axios';
 
-export const useGetNews = (pageNumber : number) => {
-  
+export const useGetNews = (pageNumber: number) => {
+  return useQuery(['get-news', pageNumber], () => getNews(pageNumber));
 };
 
-export const useGetNewsByKeyword = (keyword: string) =>{
+export const useGetNewsByKeyword = (keyword: string) => {
+  return useQuery(['get-news-by-keyword', keyword], () => getNewsByKeyword);
+};
 
-}
-
-export const useGetRecommendedNews = ()=>{
-
-}
+export const useGetRecommendedNews = () => {
+  return useQuery(['get-recommend-news'], () => getRecommendNews);
+};
